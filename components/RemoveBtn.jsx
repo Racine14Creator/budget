47c316db860f1e13a05190e03db64fa73849be66
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-03-13 16:19:17 +0200

    add: adding the id to function

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -5,14 +5,14 @@ import { useRouter } from "next/navigation"
 // const url = "http://localhost:3000/api/budget/"
 const url = "https://budget-beta-ten.vercel.app/api/budget"
 
-const RemoveBtn = () => {
+const RemoveBtn = ({ id }) => {
   const router = useRouter()
 
   const removeBudget = async function () {
     const confirmed = confirm("Do you want to delete this...")
 
     if (confirmed) {
-      const res = await fetch(`${url}/`, { method: "DELETE" })
+      const res = await fetch(`${url}/?id=${id}`, { method: "DELETE" })
 
       if (res.ok) router.refresh()
     }
