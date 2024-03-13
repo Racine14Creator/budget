aad32f7d7c9cb9ece3b0490797cd273167886dc3
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-03-13 16:35:42 +0200

    add: structure from better

--- app/api/budget/[id]/route.js
+++ app/api/budget/[id]/route.js
@@ -2,34 +2,54 @@ import DBconnection from "@/app/libs/mongodb.config";
 import Budget from "@/app/models/data.model";
 import { NextResponse } from "next/server";
 
-export async function GET(req, { params }) {
-
-    const { id } = params
-
-    await DBconnection();
-
-    const budget = await Budget.findById(id)
-
-    if (budget) {
-        return NextResponse.json({ budget }, { status: 200 })
-    } else {
-        return NextResponse.json({ message: "We can't find this..." }, { status: 201 })
+// Middleware to set CORS headers
+const allowCors = (handler) => async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
     }
-}
+    return handler(req, res);
+};
 
-export async function PUT(req, { params }) {
-
-    const { id } = params
-
-    await DBconnection()
-
-    const { amount, event, devise, desc, country } = await req.json()
-
-    const budget = await Budget.findByIdAndUpdate(id, { amount, event, devise, description: desc, country })
-
-    if (budget) {
-        return NextResponse.json({ budget }, { status: 201 })
+export default allowCors(async (req, res) => {
+    if (req.method === "GET") {
+        const { id } = req.query;
+        await DBconnection();
+        const budget = await Budget.findById(id);
+        if (budget) {
+            return NextResponse.json({ budget }, { status: 200 });
+        } else {
+            return NextResponse.json(
+                { message: "We can't find this..." },
+                { status: 404 }
+            );
+        }
+    } else if (req.method === "PUT") {
+        const { id } = req.query;
+        await DBconnection();
+        const { amount, event, devise, desc, country } = await req.body;
+        const budget = await Budget.findByIdAndUpdate(id, {
+            amount,
+            event,
+            devise,
+            description: desc,
+            country,
+        });
+        if (budget) {
+            return NextResponse.json({ budget }, { status: 200 });
+        } else {
+            return NextResponse.json(
+                { message: "Something went wrong" },
+                { status: 500 }
+            );
+        }
     } else {
-        return NextResponse.json({ message: "Something went wrong" })
+        return NextResponse.error(
+            new Error(`Method ${req.method} Not Allowed`),
+            { status: 405 }
+        );
     }
-}
+});
