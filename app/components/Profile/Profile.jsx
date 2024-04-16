7418a28939c145d911d1271e98405c3b7cdee6b2
racine14creator <bisimwagrace596@gmail.com>
2024-04-16 10:52:11 +0200

    add: profile alt image

--- app/components/Profile/Profile.jsx
+++ app/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
             <h3 className="text-3xl font-bold my-3">Profile</h3>
             <div className="flex justify-between sm:flex-col sm:p-5 max-w-md mx-auto">
                 <div className="flex justify-between items-center p-5 rounded-full bg-white text-black shadow-md my-3">
-                    <Image src={'/next.svg'} height={50} width={50} className='relative w-50 h-70 rounded-full shadow-lg' />
+                    <Image src={'/next.svg'} height={50} width={50} alt='Profile' className='relative w-50 h-70 rounded-full shadow-lg' />
                     <p className="text-xl font-bold">Profile</p>
                 </div>
                 <div className="flex justify-between items-center sm:p-5 rounded-full bg-white text-black shadow-md my-3 sm:w-full">
