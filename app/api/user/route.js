69788079de9be757749fe1f5f61134de57a4a759
racine14creator <bisimwagrace596@gmail.com>
2024-05-11 22:34:25 +0200

    add: user model and other branch

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,39 +1,44 @@
-
 // GET
 
 import DBconnection from "@/app/libs/mongodb.config";
-import User from "@/app/models/User.module";
+import User from "@/app/models/User.model";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    await DBconnection()
+export async function GET() {
+  await DBconnection();
 
-    const users = await User.find()
+  const users = await User.find();
 
-    return NextResponse.json({users}, {status: 200})
+  return NextResponse.json({ users }, { status: 200 });
 }
 
 /**
  * Post
  */
-export async function POST(req){
-    return NextResponse.json({message: "Psot req"})
+export async function POST(req) {
+  return NextResponse.json({ message: "Post req" });
 }
 /**
  * DELETE
  */
 
-export async function DELETE(req){
-    const id = req.nextUrl.searchParams.get('id')
-    await DBconnection()
-    const user = await User.findByIdAndDelete(id)
+export async function DELETE(req) {
+  const id = req.nextUrl.searchParams.get("id");
+  await DBconnection();
+  const user = await User.findByIdAndDelete(id);
 
-    if(user){
-        return NextResponse.json({message: "User deleted"}, {status: 201})
-    }else{
-        return NextResponse.json(
-            { message: "Something went wrong..." },
-            { status: 500 }
-        );
-    }
+  if (user) {
+    return NextResponse.json({ message: "User deleted" }, { status: 201 });
+  } else {
+    return NextResponse.json(
+      { message: "Something went wrong..." },
+      { status: 500 }
+    );
+  }
 }
+
+/**
+ *
+ * Check email
+ *
+ */
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,6 +1,16 @@
+"use client";
 
-import { Dashboard } from "../components/Dashboard/Dashboard"
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { Dashboard } from "../components/Dashboard/Dashboard";
 
-export default function HomePageAuth(){
-    return <Dashboard/>
+export default function HomePageAuth() {
+  const { user, isLoading } = useKindeBrowserClient();
+
+  return isLoading ? (
+    <div className='absolute top-0 left-0 w-full h-screen bg-black flex justify-center items-center flex-wrap'>
+      <span className='max-w-md loading loading-bars loading-lg mx-auto'></span>
+    </div>
+  ) : (
+    <Dashboard />
+  );
 }
