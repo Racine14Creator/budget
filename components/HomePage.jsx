7862e031267b145a295256a5f633fbb7c9c4efd0
Elijah <elijahbus.ny@gmail.com>
2024-06-21 00:54:01 +0200

    Auth 2

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,15 +1,34 @@
-import { Dashboard } from "@/app/components/Dashboard/Dashboard";
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
-import IndexPage from "./IndexPage";
+import { Dashboard } from '@/app/components/Dashboard/Dashboard';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import IndexPage from './IndexPage';
+import { log } from 'next/dist/server/typescript/utils';
+import User from '@/app/models/User.module';
+import DBconnection from '@/app/libs/mongodb.config';
 
+export default async function HomePage() {
 
-export default async function HomePage(){
+  const { getUser } = getKindeServerSession();
 
-  const {isAuthenticated} = getKindeServerSession()
+  await DBconnection();
 
-  return (await isAuthenticated() ? (
-  <Dashboard/>
-) : (
-<IndexPage/>
-))
+  const authUser = await getUser();
+  if (!authUser) {
+    return <IndexPage/>;
+  }
+
+  const foundUser = await User.findOne({ email: authUser.email });
+  console.log('Found user ', foundUser);
+
+  if (!foundUser) {
+    await User.create({
+      name: `${authUser.family_name} ${authUser.given_name}`,
+      picture: authUser.picture,
+      email: authUser.email,
+      password: '123',
+    });
+
+    return <Dashboard/>;
+  } else {
+    return <Dashboard/>;
+  }
 }
