abf7a656e5cb1e423e90a7c86c0e549788edfad4
Bisimwa Grace <bisimwagrace596@gmail.com>
2024-09-02 07:59:47 +0200

    Navbar

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -129,10 +129,10 @@ export default function Navbar() {
               ) : (
                 <>
                   <LoginLink className='bg-blue-500 text-white hover:bg-gray-700 hover:text-white/50 mr-3 rounded-md px-3 py-2 text-sm font-medium'>
-                    Sign In
+                    Se connecter
                   </LoginLink>
                   <RegisterLink className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                    Sign Up
+                    Creer un compte
                   </RegisterLink>
                 </>
               )}
